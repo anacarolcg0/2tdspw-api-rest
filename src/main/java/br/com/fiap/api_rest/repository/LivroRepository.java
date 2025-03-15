077c481e0d81600579041d7e6caba672e0dd9169
anacarolcg0 <anacarolcg0@gmail.com>
2025-03-14 23:03:07 -0300

    acrescentando...

--- src/main/java/br/com/fiap/api_rest/repository/LivroRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/LivroRepository.java
@@ -1,9 +1,39 @@
 package br.com.fiap.api_rest.repository;
 
 import br.com.fiap.api_rest.model.Livro;
+
+import java.sql.Date;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+    List<Livro> findByTitulo(String titulo);
+    List<Livro> findTop18ByPrecoAndDataLancamentoGreaterThan(Date dataLancamento);
+    Livro findFirstByAutor(String autor);
+    List<Livro> findDistinctByTituloNotIn(List<String> titulos);
+    List<Livro> findByTituloIs(String titulo);
+    List<Livro> findByTituloEqualsIgnoreCase(String titulo);
+    List<Livro> findByTituloIsNot(String titulo);
+    List<Livro> findByIsbnIsNotNull();
+    List<Livro> findByEbookTrue();
+    List<Livro> findByEbookFalse();
+    List<Livro> findByAutorStartingWith(String prefix);
+    List<Livro> findByTituloEndingWith(String suffix);
+    List<Livro> findByTituloContaining(String infix);
+    List<Livro> findByTituloLike(String likePattern);
+    List<Livro> findByPrecoLessThanEqual(double preco);
+    List<Livro> findByPrecoGreaterThanEqual(double preco);
+    List<Livro> findByPrecoBetween(double min, double max);
+    List<Livro> findByTituloIn(Collection<String> livrosBaratos);
+    List<Livro> findByDataLancamentoBefore(Date dataLancamento);
+    List<Livro> findByDataLancamentoAfter(Date dataLancamento);
+    List<Livro> findByDataLancamentoBetween(Date dataLancamentoInicial, Date dataLancamentoFinal);
+    List<Livro> findByTituloOrAutor(String titulo, String autor);
+    List<Livro> findByTituloOrderByTituloAsc(String titulo);
+    List<Livro> findByTituloOrderByTituloDesc(String titulo);
+
 }
