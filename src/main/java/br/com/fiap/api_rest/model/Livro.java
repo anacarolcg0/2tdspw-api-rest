9a0c28f3621096c6547c57b1be0db6893629bf07
anacarolcg0 <anacarolcg0@gmail.com>
2025-03-18 22:58:29 -0300

    adicionando...

--- src/main/java/br/com/fiap/api_rest/model/Livro.java
+++ src/main/java/br/com/fiap/api_rest/model/Livro.java
@@ -1,11 +1,9 @@
 package br.com.fiap.api_rest.model;
 
 import java.sql.Date;
+import java.util.List;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Livro {
@@ -13,12 +11,16 @@ public class Livro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
-    private String autor;
+    @ManyToMany(mappedBy = "livros")
+    private List<Autor> autores;
     private int preco;
     private Categoria categoria;
     private String isbn;
     private boolean ebook;
     private Date dataLancamento;
+    @ManyToOne
+    @JoinColumn(name = "id_biblioteca")
+    private Biblioteca biblioteca;
 
     public Long getId() {
         return id;
@@ -36,12 +38,12 @@ public class Livro {
         this.titulo = titulo;
     }
 
-    public String getAutor() {
-        return autor;
+    public List<Autor> getAutores() {
+        return autores;
     }
 
-    public void setAutor(String autor) {
-        this.autor = autor;
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
     }
 
     public int getPreco() {
@@ -84,5 +86,11 @@ public class Livro {
         this.dataLancamento = dataLancamento;
     }
 
-    
+    public Biblioteca getBiblioteca() {
+        return biblioteca;
+    }
+
+    public void setBiblioteca(Biblioteca biblioteca) {
+        this.biblioteca = biblioteca;
+    }
 }
